3caf472a1933f630ffaad227954f920e12a8e796
oshiro yuji <poriteku.09oshiro@gmail.com>
2022-09-05 13:43:11 +0900

    BrainにRealTimeClockをインクルードとインスタンス生成

--- arduino_projects/honban/Brain/Brain.ino
+++ arduino_projects/honban/Brain/Brain.ino
@@ -1,10 +1,12 @@
 #include "Lid.h"
-#include "UltraSonicSensor"
+#include "UltraSonicSensor.h"
+#include "RealTimeClock.h"
 /*Lidクラスインスタンス生成*/
 Lid lid = Lid(4,5); //(4,5)仮
 /*超音波センサーインスタンス生成*/
 UltraSonicSensor ultrasonicsensor = UltraSonicSensor(13,12);//(13,12)仮
-
+/*RealTimeClockインスタンス生成*/
+RealTimeClock realtimeclock = new RealTimeClock();
 
 void setup() {
   
