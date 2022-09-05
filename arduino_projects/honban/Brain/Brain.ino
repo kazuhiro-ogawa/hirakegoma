335d79cbe0acfa9cd966e98bdd0964a0a7344458
oshiro yuji <poriteku.09oshiro@gmail.com>
2022-09-05 13:48:56 +0900

    RealTimeClockインスタンス生成微修正

--- arduino_projects/honban/Brain/Brain.ino
+++ arduino_projects/honban/Brain/Brain.ino
@@ -6,7 +6,7 @@ Lid lid = Lid(4,5); //(4,5)仮
 /*超音波センサーインスタンス生成*/
 UltraSonicSensor ultrasonicsensor = UltraSonicSensor(13,12);//(13,12)仮
 /*RealTimeClockインスタンス生成*/
-RealTimeClock realtimeclock = new RealTimeClock();
+RealTimeClock realtimeclock;
 
 void setup() {
   
