32988001de018e39fe6a129735306fd15340de38
oshiro yuji <poriteku.09oshiro@gmail.com>
2022-09-06 14:15:12 +0900

    人感センサー追加

--- arduino_projects/honban/BrainMotor/BrainMotor.ino
+++ arduino_projects/honban/BrainMotor/BrainMotor.ino
@@ -2,6 +2,9 @@
 #include "UltraSonicSensor.h"
 #include"Speaker.h"
 
+/*人感センサーピン仮設定
+#define humanSensor_IN 8*/
+
 /*Lidクラスインスタンス生成*/
 Lid lid = Lid(25,26); 
 /*超音波センサーインスタンス生成*/
@@ -9,11 +12,13 @@ UltraSonicSensor ultrasonicsensor = UltraSonicSensor(17,16);
 /*スピーカーのインスタンス生成*/
 Speaker speaker = Speaker(33);
 void setup() {
+  //人感センサー設定
+  pinMode(humanSensor_IN,INPUT);
    //サーボ初期設定
   lid.init();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 
 }
