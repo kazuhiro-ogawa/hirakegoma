8e599e3f1fbd1eed4d6e711782a8c26d8f127028
oshiro yuji <poriteku.09oshiro@gmail.com>
2022-09-06 14:43:33 +0900

    人感センサー追加

--- arduino_projects/honban/BrainSensor/BrainSensor.ino
+++ arduino_projects/honban/BrainSensor/BrainSensor.ino
@@ -2,6 +2,8 @@
 #include "Display.h"
 #include <Wire.h>
 #include "RealTimeClock.h"
+/*人感センサーピン仮設定
+#define humanSensor_IN 0*/
 
 /*RealTimeClockインスタンス生成*/
 RealTimeClock realtimeclock;
@@ -9,8 +11,14 @@ RealTimeClock realtimeclock;
 Display display;
 /*Angleインスタンス生成*/
 Angle angle;
+/*ブルートゥースから信号を受け取るメソッド*/
+void mode_change(){
+  
+}
 
 void setup() {
+   //人感センサー設定
+  attachInterrupt(PIN,mode_change,RISING);//割り込み
   // Lcd初期化処理
   display.init();
 
