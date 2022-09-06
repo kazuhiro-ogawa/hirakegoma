4e9305da19fee788a4b7c398eb6ce39100679b61
oshiro yuji <poriteku.09oshiro@gmail.com>
2022-09-06 14:44:00 +0900

    人感センサー消去

--- arduino_projects/honban/BrainMotor/BrainMotor.ino
+++ arduino_projects/honban/BrainMotor/BrainMotor.ino
@@ -2,8 +2,7 @@
 #include "UltraSonicSensor.h"
 #include"Speaker.h"
 
-/*人感センサーピン仮設定
-#define humanSensor_IN 8*/
+
 
 /*Lidクラスインスタンス生成*/
 Lid lid = Lid(25,26); 
@@ -11,9 +10,8 @@ Lid lid = Lid(25,26);
 UltraSonicSensor ultrasonicsensor = UltraSonicSensor(17,16);
 /*スピーカーのインスタンス生成*/
 Speaker speaker = Speaker(33);
+
 void setup() {
-  //人感センサー設定
-  pinMode(humanSensor_IN,INPUT);
    //サーボ初期設定
   lid.init();
 }
